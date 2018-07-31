2fd52d7787f72c1026ec1eaef33b3af8ace8e5bb
lvrui <lvrui03@126.com>
2018-07-31 22:15:59 +0800

    support gunicorn and update logger

--- robot/main.py
+++ robot/main.py
@@ -1,33 +1,44 @@
 import asyncio
 import uvloop
-import logging
 from aiohttp import web
 from routes import setup_routes
 from config import robot_cfg as cfg
 from utils.session_helper import Session
 from utils.db_helper import MySQLConnector
-from utils.logger_helper import slog
+from utils.logger_helper import LogFactory
+
+logger = LogFactory.get_logger()
 
 
 async def shutdown(app):
-    print("Shutting Down The Server...")
+    logger.info("Shutting Down The Server...")
 
-    print("Close Session...")
+    logger.info("Close Session...")
     await Session.close_all_session()
-    print("Close Session, Done")
+    logger.info("Close Session, Done")
 
-    print("Close MySQL Connect Pool...")
+    logger.info("Close MySQL Connect Pool...")
     await MySQLConnector.close_conn()
-    print("Close MySQL Connect Pool, Done")
+    logger.info("Close MySQL Connect Pool, Done")
 
 
 def main():
-    logging.basicConfig(filename='logs/robot.log', level=logging.DEBUG)
-    slog.info("Server Starting at {0};{1}".format(cfg.host, cfg.port))
+    logger.info("Server Starting at {0};{1}".format(cfg.host, cfg.port))
     app = web.Application()
     setup_routes(app)
     app.on_shutdown.append(shutdown)
     web.run_app(app, host=cfg.host, port=cfg.port)
+
+
+# For Gunicorn
+async def web_app():
+    # 使用 uvloop 替换掉 asyncio 默认的事件循环
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    logger.info("Server Starting at {0};{1}".format(cfg.host, cfg.port))
+    app = web.Application()
+    setup_routes(app)
+    app.on_shutdown.append(shutdown)
+    return app
 
 
 if __name__ == "__main__":
@@ -38,4 +49,4 @@ if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-        slog.error(e)
+        logger.error(e)
